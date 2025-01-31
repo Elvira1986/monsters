b6177361b22c1a566c71821d7c8678adc6fba20d
Elvira Gizatullina <elvirawed@gmail.com>
2025-01-30 22:19:25 -0800

    Transfering to TypeScript app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+import React from "react";
+
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// This is the ID of the div in your index.html file
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
